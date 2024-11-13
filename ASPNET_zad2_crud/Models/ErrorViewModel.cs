3a1683154cc8d73460959729ed3f04cfcbd5a678
dawid <dawid@KOMPIK-JUSTYNA>
2024-11-13 21:42:05 +0100

    Dodaj pliki projektów.

---
+++ ASPNET_zad2_crud/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ASPNET_zad2_crud.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
